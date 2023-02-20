836a876c4c784ae8492e03502a6ec31b69a34228
SanskarPoudel <72004392+SanskarPoudel@users.noreply.github.com>
2023-02-20 14:55:12 +0545

    Welcome screen done

--- app/src/main/java/com/example/photoeditor/MainActivity.java
+++ app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.photoeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!MainActivity.this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            findViewById(R.id.takePhotoButton).setVisibility(View.GONE);
+        }
+
+    }
+    @Override
+    protected  void  onResume(){
+        super.onResume();
+    }
+
+    private void init(){
+        final Button selectImageButton  = findViewById(R.id.selectImageButton);
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        final Button takePhotoButton = findViewById(R.id.takePhotoButton);
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
